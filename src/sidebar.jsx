0349670b36110a96a958b9fd5cb4d9423af58930
Mohana Murthy <mohanmovin22@gmail.com>
2026-07-01 15:30:00 +0530

    added chmages in sidebar

---
+++ src/sidebar.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+function Sidebar() {
+    return(
+        <div className='sidebar'>
+            <div className='sidebar-header'>
+            <h2>The Registry</h2>
+            <p>Welcome to the Registry</p>
+        </div>
+        </div>
+    )
+}
